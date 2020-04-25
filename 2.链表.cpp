54142fd45facfea60f35ce4cbcc6ca4a346c7fbf
阿诺兹 <anotts@anuozideMBP.lan>
2020-04-25 14:49:06 +0800

    改名

---
+++ 2.链表.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+// https://leetcode-cn.com/problems/add-two-numbers/
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    
+}
